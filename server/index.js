fee83e5c1fd91d5c17d71a3adc054ce349ceecc4
Tushar Anand <tushar.nasya@gmail.com>
2023-11-28 10:17:23 +0530

    database connected

--- server/index.js
+++ server/index.js
@@ -1,16 +1,20 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
 const app = express();
-
+dotenv.config();
 const PORT = 8080;
 
-app.get('/',(req,res)=>{
-    res.json({
-        message:"API working",
-        success: true
+const connectDB = async() =>{
+    await mongoose.connect(process.env.MONGO_URL).then(()=>{
+        console.log('DB Connected')
+    }).catch((error)=>{
+        console.log(error)
     })
-})
+}
 
 app.listen(PORT,()=>{
     console.log("http://localhost:8080")
+    connectDB();
 })
